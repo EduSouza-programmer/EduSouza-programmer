2a5703f643499c4a9d5e18abce50200c5b6d2521
Eduardo Souza <dado.eletrons@gmail.com>
2021-08-22 21:47:08 -0300

    refactory

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in 1 2 3 4 5; do
   echo "counter: $i" >> check.txt
-  
-  git add . && git commit -m "test"
- 	
+  git add . && git commit -m "refactory"	
 done
+
+git push origin main
