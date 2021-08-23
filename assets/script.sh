ccf62261f16355e339257e17154f0f4ff23abd7f
Eduardo Souza <dado.eletrons@gmail.com>
2021-08-23 09:41:03 -0300

    test3

--- assets/script.sh
+++ assets/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for i in 1 2 3 ; do
+for i in {1...$1} ; do
   echo "counter: $i" >> assets/check.txt
-  git add . && git commit -m "test2"	
+  git add . && git commit -m "test3"	
 done
 
 git push origin main
